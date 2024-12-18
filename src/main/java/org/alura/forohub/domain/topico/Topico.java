cfc534c071c20c85004df1488ed3751a860271d8
Alonso Bonifacio <alonso.aybp@gmail.com>
2024-12-18 13:25:12 -0500

    Add Topico entity and database migration script
    Introduce the `Topico` entity with associated fields and relationships, including mapping to the `usuarios` table for the `autor` field. Add a new database migration script to create the `topico` table with appropriate constraints and foreign keys.

---
+++ src/main/java/org/alura/forohub/domain/topico/Topico.java
@@ -0,0 +1,31 @@
+package org.alura.forohub.domain.topico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.alura.forohub.domain.usuarios.Usuario;
+
+import java.time.LocalDateTime;
+
+@Table(name = "topico")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private LocalDateTime fechaCreacion;
+    private boolean status;
+    private String curso;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id")
+    private Usuario autor;
+}
---
+++ src/main/resources/db/migration/V2__create-table-topico.sql
@@ -0,0 +1,13 @@
+create table topico(
+    id bigint not null auto_increment,
+    titulo varchar(100) not null,
+    mensaje varchar(300) not null,
+    fecha_creacion datetime not null,
+    status tinyint not null,
+    autor_id bigint not null,
+    curso varchar(100) not null,
+
+
+    primary key(id),
+    constraint fk_topico_autor_id foreign key(autor_id) references usuarios(id)
+);
